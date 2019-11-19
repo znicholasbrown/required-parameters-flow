7a0958d13831145b741ca2ff4fdda75edc1feb20
nicholas <znicholasbrown@gmail.com>
2019-11-19 11:59:02 -0500

    Remove random_word task

--- Required_Flow.py
+++ Required_Flow.py
@@ -24,24 +24,27 @@ def words_query():
         random_index = randrange(words_length)
         print(f"\t\t{words[random_index]}")
 
-@task(name="Get Random Word")
 def random_word():
     return words[randrange(len(words))]
 
 @task(name="Print Required Parameters")
 def print_params(task_params):
-    print("\Printing required parameters!")
+    print(f"\tPrinting required parameters!")
     for arg in task_params:
         print(arg)
 
 @task(name="Make Required Parameters")
 def make_params():
-    print("\tMaking required parameters!")
+    global parameter_map
+    print(f"\tMaking required parameters!")
     for i in range(50):
-        parameter_map[i] = Parameter(random_word(), default=randrange(10000), required=True)
+        word = random_word()
+        val = randrange(10000)
+        print(f"\t\tParameter {i}: {word}, {val}")
+        parameter_map[i] = Parameter(word, default=val, required=True)
 
 
-with Flow("Required Parameters Flow") as RequiredParameters_Flow:
+with Flow("Required Parameters Flow") as RequiredParameters_Flow:    
     query_task = words_query()
 
     make_task = make_params(upstream_tasks=[query_task])
@@ -49,14 +52,12 @@ with Flow("Required Parameters Flow") as RequiredParameters_Flow:
     print_task = print_params(parameter_map, upstream_tasks=[query_task, make_task])
     print(print_task)
 
-RequiredParameters_Flow.run()
 
-
-# RequiredParameters_Flow.deploy(
-#     "Flow Schematics", 
-#     base_image="python:3.7",
-#     python_dependencies=[],
-#     registry_url="znicholasbrown",
-#     image_name="prefect_flow",
-#     image_tag="required-parameters-flow",
-# )
+RequiredParameters_Flow.deploy(
+    "Flow Schematics", 
+    base_image="python:3.7",
+    python_dependencies=[],
+    registry_url="znicholasbrown",
+    image_name="prefect_flow",
+    image_tag="required-parameters-flow",
+)
