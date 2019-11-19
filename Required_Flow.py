e84770556b0a286ab7295056a80c82ab4c1bad17
nicholas <znicholasbrown@gmail.com>
2019-11-19 11:42:18 -0500

    Update query word task

--- Required_Flow.py
+++ Required_Flow.py
@@ -1,23 +1,32 @@
 from prefect import Flow, task, Parameter
 import urllib.request
-from random import uniform
+from random import randrange
 
 parameter_map = {}
 words=[]
 
 @task(name="Query for Words")
 def words_query():
+    global words
     if (len(words) > 0):
-        return words
+        return
     
     url = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
     response = urllib.request.urlopen(url)
     text = response.read().decode()
     words = text.splitlines()
 
+    words_length = len(words)
+
+    print(f"\t50 random words from the dictionary: ")
+    # we'll print 50 random words
+    for i in range(50):
+        random_index = randrange(words_length)
+        print(f"\t\t{words[random_index]}")
+
 @task(name="Get Random Words")
 def random_words():
-    return words[uniform(0, len(words))]
+    return words[randrange(len(words))]
 
 @task(name="Print Required Parameters")
 def print_params(task_params):
@@ -36,14 +45,16 @@ with Flow("Required Parameters Flow") as RequiredParameters_Flow:
     make_task = make_params(upstream_tasks=[query_task])
 
     print_task = print_params(parameter_map, upstream_tasks=[query_task, make_task])
-    
+    print(print_task)
+
+RequiredParameters_Flow.run()
 
 
-RequiredParameters_Flow.deploy(
-    "Flow Schematics", 
-    base_image="python:3.7",
-    python_dependencies=[],
-    registry_url="znicholasbrown",
-    image_name="prefect_flow",
-    image_tag="required-parameters-flow",
-)
+# RequiredParameters_Flow.deploy(
+#     "Flow Schematics", 
+#     base_image="python:3.7",
+#     python_dependencies=[],
+#     registry_url="znicholasbrown",
+#     image_name="prefect_flow",
+#     image_tag="required-parameters-flow",
+# )
